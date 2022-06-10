56d251d4f836ed7965619ae348635d45c4bd6d5a
Gonzalo Villafañe Tapia <gvillafanetapia@gmail.com>
2022-06-10 19:12:41 -0300

    feat(incremental-merge-strategy): partition pruning

---
+++ dbt/include/databricks/macros/materializations/incremental/strategies.sql
@@ -0,0 +1,23 @@
+{% macro dbt_spark_get_incremental_sql(strategy, source, target, unique_key, partition_by, partitions=[]) %}
+  
+  {% set partition_match %}
+    {{ target }}.{{ partition_by }} IN ({% for p in partitions -%}"{{ p[partition_by] }}"{% if !loop.last -%},{%- endif -%} {%- endfor -%})
+  {% endset %}
+  {%- set predicates = [partition_match] -%}
+
+  {%- if strategy == 'append' -%}
+    {#-- insert new records into existing table, without updating or overwriting #}
+    {{ get_insert_into_sql(source, target) }}
+  {%- elif strategy == 'insert_overwrite' -%}
+    {#-- insert statements don't like CTEs, so support them via a temp view #}
+    {{ get_insert_overwrite_sql(source, target) }}
+  {%- elif strategy == 'merge' -%}
+  {#-- merge all columns with databricks delta - schema changes are handled for us #}
+    {{ get_merge_sql(target, source, unique_key, dest_columns=none, predicates=predicates) }}
+  {%- else -%}
+    {% set no_sql_for_strategy_msg -%}
+      No known SQL for the incremental strategy provided: {{ strategy }}
+    {%- endset %}
+    {%- do exceptions.raise_compiler_error(no_sql_for_strategy_msg) -%}
+  {%- endif -%}
+{% endmacro %}
