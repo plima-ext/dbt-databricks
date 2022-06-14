e711260d4fe35b9180e23b0a3fc03608c7b19bf6
Gonzalo Villafañe Tapia <gvillafanetapia@gmail.com>
2022-06-14 18:37:26 -0300

    override get_merge_sql to debug

--- dbt/include/databricks/macros/materializations/incremental/strategies.sql
+++ dbt/include/databricks/macros/materializations/incremental/strategies.sql
@@ -1,3 +1,54 @@
+{% macro dbt_databricksv__get_merge_sql(target, source, unique_key, dest_columns, predicates) -%}
+    {{- log("using dbt_databricksv__get_merge_sql...", True) -}}
+    
+    {%- set predicates = [] if predicates is none else [] + predicates -%}
+    {%- set dest_cols_csv = get_quoted_csv(dest_columns | map(attribute="name")) -%}
+    {%- set update_columns = config.get('merge_update_columns', default = dest_columns | map(attribute="quoted") | list) -%}
+    {%- set sql_header = config.get('sql_header', none) -%}
+
+    {{- log("predicates: " ~predicates, True) -}}
+
+    {% if unique_key %}
+        {% if unique_key is sequence and unique_key is not mapping and unique_key is not string %}
+            {% for key in unique_key %}
+                {% set this_key_match %}
+                    DBT_INTERNAL_SOURCE.{{ key }} = DBT_INTERNAL_DEST.{{ key }}
+                {% endset %}
+                {% do predicates.append(this_key_match) %}
+            {% endfor %}
+        {% else %}
+            {% set unique_key_match %}
+                DBT_INTERNAL_SOURCE.{{ unique_key }} = DBT_INTERNAL_DEST.{{ unique_key }}
+            {% endset %}
+            {% do predicates.append(unique_key_match) %}
+        {% endif %}
+    {% else %}
+        {% do predicates.append('FALSE') %}
+    {% endif %}
+
+    {{- log("predicates: " ~predicates, True) -}}
+
+    {{ sql_header if sql_header is not none }}
+
+    merge into {{ target }} as DBT_INTERNAL_DEST
+        using {{ source }} as DBT_INTERNAL_SOURCE
+        on {{ predicates | join(' and ') }}
+
+    {% if unique_key %}
+    when matched then update set
+        {% for column_name in update_columns -%}
+            {{ column_name }} = DBT_INTERNAL_SOURCE.{{ column_name }}
+            {%- if not loop.last %}, {%- endif %}
+        {%- endfor %}
+    {% endif %}
+
+    when not matched then insert
+        ({{ dest_cols_csv }})
+    values
+        ({{ dest_cols_csv }})
+
+{% endmacro %}
+
 {% macro dbt_databricks_get_insert_overwrite_sql(source_relation, target_relation) %}
 
     {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
@@ -23,13 +74,13 @@
   {%- set predicates = [] -%}
   {% if partitions %}
     {% set partition_match %}
-      {% for partition_key in partition_by -%}
+      {%- for partition_key in partition_by -%}
       {{- log("checking partition key: " ~partition_key, True) -}}
         {% if partitions[partition_key] %}
         {%- if loop.first %}({% endif -%}
-        {% for partition_values in partitions[partition_key] -%}
+        {%- for partition_values in partitions[partition_key] -%}
           {{- log("partition_values: " ~partition_values, True) -}}
-          {%- if loop.first %}({% endif -%}{{ target }}.partition_key = "{{ partition_values[0] }}"{%- if not loop.last %} AND{% else -%}){% endif -%}
+          {%- if loop.first %}({% endif -%}{{ target }}.{{ partition_key }} = "{{ partition_values[0] }}"{%- if not loop.last %} AND{% else -%}){% endif -%}
         {%- endfor -%}
         {%- if not loop.last %} OR{% else -%}){% endif -%}
         {% endif %}
