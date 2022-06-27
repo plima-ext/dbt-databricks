075572e61ef49d2e58c501924bf494fea5e633cd
Gonzalo Villafañe Tapia <gvillafanetapia@gmail.com>
2022-06-27 20:43:31 -0300

    fix(incremental strategy): partition conditions invert partition keys loops order

--- dbt/include/databricks/macros/materializations/incremental/strategies.sql
+++ dbt/include/databricks/macros/materializations/incremental/strategies.sql
@@ -62,14 +62,14 @@
   {%- set predicates = [] -%}
   {% if partitions %}
     {% set partition_match %}
-      {%- for partition_key in partition_by -%}
-        {% if partitions[partition_key] %}
+      {%- for partition_values in partitions -%}
         {%- if loop.first %}({% endif -%}
-        {%- for partition_values in partitions[partition_key] -%}
+        {%- for partition_key in partition_by -%}
+          {% if partitions[partition_key] %}
           {%- if loop.first %}({% endif -%}DBT_INTERNAL_DEST.{{ partition_key }} = "{{ partition_values[0] }}"{%- if not loop.last %} AND {% else -%}){% endif -%}
+          {% endif %}
         {%- endfor -%}
         {%- if not loop.last %} OR {% else -%}){% endif -%}
-        {% endif %}
       {%- endfor -%}
     {% endset %}
     {% if partition_match %}
