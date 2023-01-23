34d4a880244c9645249149026cad039b9ff8f641
Paula Lima <plima-ext@addi.com>
2023-01-23 13:32:21 -0300

    test(incremental): remove comment

--- dbt/include/databricks/macros/materializations/incremental/incremental.sql
+++ dbt/include/databricks/macros/materializations/incremental/incremental.sql
@@ -31,6 +31,9 @@
   {% if existing_relation is none %}
     {% set build_sql = create_table_as(False, target_relation, sql) %}
   {% elif existing_relation.is_view or full_refresh_mode %}
+    {% if not is_delta %}
+      {% do adapter.drop_relation(existing_relation) %}
+    {% endif %}
     {% set build_sql = create_table_as(False, target_relation, sql) %}
   {% else %}
     {% do run_query(create_table_as(True, tmp_relation, sql)) %}
